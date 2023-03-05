5f5296209b5c9d2f30ce320e1249939cc9f5b64d
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-05 16:04:04 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './styles.css';
+import logoImageSrc from './images/ReactLogo.png'
+
+// add .svg as react component
+import { ReactComponent as Logo } from './images/logo.svg'
 
 // const el = React.createElement(
 //   'h1',
@@ -37,51 +42,63 @@ import { createRoot } from 'react-dom/client';
 
 // const Appheader = () => {
 //   return <h1><span>AppHeader</span></h1>
-
 // }
 
-// const el = (
+// const App = () => {
+//  return (
 //   <>
 //     <Appheader />
 //     <AppList />
-
 //   </>
-// );
+//  )
+// }
 
 // const rootElement = document.getElementById('root');
 // const root = createRoot(rootElement);
 
-// root.render(el);
+// root.render(<App/>);
 
-// 44444444444444444444444444444444444444444444444444
-
+// 4444444444 передача аргумента в компонент - props 44444444444444
 
 const AppList = () => {
+  const items = ['Мой первый элемент', "Мой второй элемент", "Мой третий элемент"];
+  const firstItem = <li>qqqqqqqqqqqqq</li>;
+  const isAuth = true;
+  return (
+    <ul>
+      {items.map((item, index) => {
+        return <li key={index} > {item} </li>
+      })}
+    </ul>
+  )
 
+}
 
-    const items = ['My first element', 'My second element'];
-    const firstItem = <li>Самый первый элемент</li>
-    const isAuth = true;
-    return (
-       <ul>
-          {
-             isAuth ? firstItem :null
-          }
-          {
-             items.map(item => <li>{item}</li>)
-          }
-          <li>{items[0]}</li>
-          <li>{items[1]}</li>
-       </ul>
-    )
- 
+const AppInput = ({ placeholder, label }) => {
+  return (
+    <label className='label'>
+      {label}
+      <input placeholder={placeholder} type='password' />
+    </label>
+  )
+}
 
-  }
+const Appheader = ({ title }) => {
+  return (
+    <div>
+      {/* add .svg as react component */}
+      <Logo />
 
+      {/* add images from css */}
+      <img className='imageReactLogo' src={logoImageSrc} />
 
+      <h1 className='header-title'><span>{title}</span></h1>;
 
-const Appheader = () => {
-  return <h1><span>AppHeader</span></h1>
+      {/* undefind 1.30 */}
+      {title && <h1 className='header-title'><span>{title}</span></h1>}
+    </div>
+  )
+
 
 }
 
@@ -89,16 +106,17 @@ const App = () => {
   return (
     <>
       <Appheader />
+      {/* <Appheader title = 'Привет мир!'/> */}
       <AppList />
+      {/* <Appheader title = '2Привет мир!'/> */}
+      <AppInput placeholder="Введите ваше имя" label="Имя" />
+      <AppInput placeholder="Введите ваше 2имя" label="2Имя" />
 
     </>
   )
-};
+}
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-
-root.render(<App/>);
-
-
+root.render(<App />);
