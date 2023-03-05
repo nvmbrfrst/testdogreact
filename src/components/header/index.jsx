2d787924e60c95c9858a74a7c6faa71891a054a1
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-05 18:50:20 +0300

    test

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,18 @@
+import { Logo } from '../logo';
+import { Search } from '../search';
+
 import './styles.css';
 
 export function Header() {
   return (
-    <>
-    
-    </>
+    <header className='header'>
+      <div className='container header__wrapper'>
+        <Logo />
+        <Search />
+      </div>
+
+
+    </header>
   );
 }
 
--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,10 +1,11 @@
 import './styles.css';
+import logoSrc from './assets/logo.svg';
 
 export function Logo() {
   return (
-    <>
-    
-    </>
+    <a href = '/' className='logo'>
+     <img src={logoSrc} alt="" />
+    </a>
   );
 }
 
