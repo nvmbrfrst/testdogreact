f18d5dd581acdafa4377052f3658f04b757133e7
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-05 19:00:05 +0300

    test

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -4,7 +4,7 @@ import logoSrc from './assets/logo.svg';
 export function Logo() {
   return (
     <a href = '/' className='logo'>
-     <img src={logoSrc} alt="" />
+     <img src={logoSrc} alt="Логотип компании" className = 'logo__pic/>
     </a>
   );
 }
--- src/components/search/index.jsx
+++ src/components/search/index.jsx
@@ -2,10 +2,9 @@ import './styles.css';
 
 export function Search() {
   return (
-    <>
-    
-    </>
+    < form className='search'>
+      <input type='text' className = 'search__input' placeholder = 'Поиск'/>
+    </form>
   );
 }
 
- 
