6aeeb31e9f92eead106a27b19d1a2edce44483f1
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-06 16:06:39 +0300

    test

--- src/components/card-list/index.jsx
+++ src/components/card-list/index.jsx
@@ -5,9 +5,19 @@ import "./styles.css";
 export function CardList() {
   return (
     <div className="cards content__cards">
-      {dataCard.map((dataItem) => (
-        <Card {...dataItem} />
+
+      {/* {dataCard.map(function (dataItem))
+            return <Card name={dataItem.name} price = {dataItem.price} />;
+      } */}
+
+      {/* {dataCard.map(function (dataItem, index) {
+        return <Card key={index} {...dataItem} />;
+      })} */}
+
+      {dataCard.map((dataItem, index) => (
+        <Card key={index} {...dataItem} />
       ))}
+
     </div>
   );
 }
--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,39 +1,70 @@
 import './styles.css';
 import likeIcon from '../../images/save.svg';
 
-export function Card({name, price, discount, wight, description, picture}) {
+export function Card({
+  name,
+  price,
+  discount,
+  wight,
+  description,
+  picture,
+  ...props
+}) {
+
+  const discount_price = Math.round(price - (price * discount) / 100);
 
   return (
     <article className="card">
 
-        <div className="card__sticky card__sticky_type_top-left">
+      {/* иконка - скидка */}
+      <div className="card__sticky card__sticky_type_top-left">
+        {discount !== 0 && (
           <span className="card__discount">{`-${discount}%`}</span>
+        )}
+      </div>
+
+      {/* иконка - лайк */}
+      <div className="card__sticky card__sticky_type_top-right">
+        <button className="card__favorite">
+          <img src={likeIcon} alt="" className="card__favorite-icon" />
+        </button>
+      </div>
+
+
+      {/* карточка */}
+      <a href="#" className="card__link">
+
+        {/* карточка - картинка */}
+        <img src={picture} alt={name} className="card__image" />
+
+        {/* карточка - описание */}
+        <div className="card__desc">
+
+          {/* старая цена. подмена класса */}
+          <span className={discount !== 0 ? "card__old-price" : "card__price"}>
+            {price}&nbsp;₽
+          </span>
+
+          {/* дисконт. поведение элемента */}
+          {discount !== 0 && (
+            <span className="card__price card__price_type_discount">
+              {discount_price}&nbsp;₽
+            </span>
+          )}
+
+          {/* вес */}
+          <span className="card__wight">{wight}</span>
+
+          {/* карточка - имя */}
+          <h3 className="card__name">{name}</h3>
         </div>
 
-        <div className="card__sticky card__sticky_type_top-right">
+      </a>
 
-          <button className="card__favorite">
-            <img src={likeIcon} alt="" className="card__favorite-icon" />
-          </button>
-
-        </div>
-
-        <a href="#" className="card__link">
-
-          <img src={picture} alt={name} className="card__image" />
-
-          <div className="card__desc">
-
-            <span className={discount !== 0 ? "card__old-price": "card__price"}>{price}&nbsp;₽</span>
-            {discount !== 0 && <span className="card__price card__price_type_discount">{price}&nbsp;₽</span>}
-            <span className="card__wight">{wight}</span>
-
-            <h3 className="card__name">{name}</h3>
-          </div>
-
-        </a>
-
-        <a href="#" className="card__cart btn btn_type_primary">В корзину</a>
+      {/* кнопка - корзина */}
+      <a href="#" className="card__cart btn btn_type_primary">
+        В корзину
+      </a>
 
     </article>
   );
