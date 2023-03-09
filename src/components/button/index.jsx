727a2dc4ea139b80f49ee544caf7a50a6be2e255
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-09 18:31:10 +0300

    test

---
+++ src/components/button/index.jsx
@@ -0,0 +1,15 @@
+import cn from 'classnames';
+import s from './styles.module.css';
+
+export const Button = ({ type, children, extraClass, htmlType = 'button' }) => {
+    console.log(extraClass);
+    return (
+        <button type={htmlType} className={cn(
+            s.button,
+            {
+                [s[`button_type_${type}`]]: !!s[`button_type_${type}`],
+                [extraClass]: !!extraClass
+            }
+        )}>{children}</button>
+    )
+}
