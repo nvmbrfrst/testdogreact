3f62272d67b9e78c7ed904426181fb35125b6d07
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-10 18:48:52 +0300

    test

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,40 +1,77 @@
-import { Table as AntTable} from 'antd';
 
-export const Table = () => {
+import { Image, Table as AntTable, Typography } from 'antd';
+import { pokemonData } from './data';
+import { useState } from 'react';
 
-    const dataSource = [
+const { Paragraph } = Typography;
+
+const dataPokemon = pokemonData.map(pokemon => ({
+    name: pokemon.name,
+    number: pokemon.number,
+    key: pokemon.id,
+    classification: pokemon.classification,
+    maxHP: pokemon.maxHP,
+    maxCP: pokemon.maxCP,
+    image: pokemon.image
+}))
+
+export const Table = ({ rows = 20 }) => {
+
+    const columns = [
         {
-          key: '1',
-          name: 'Mike',
-          age: 32,
-          address: '10 Downing Street',
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <Paragraph copyable>{text}</Paragraph>
         },
         {
-          key: '2',
-          name: 'John',
-          age: 42,
-          address: '10 Downing Street',
-        },
-      ];
-      
-      const columns = [
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
+            title: 'Number',
+            dataIndex: 'number',
+            key: 'number',
         },
         {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
+            title: 'Class',
+            dataIndex: 'classification',
+            key: 'classification',
+            filters: [
+                {
+                    text: 'Seed Pokémon',
+                    value: 'Seed Pokémon',
+                },
+                {
+                    text: 'Lizard Pokémon',
+                    value: 'Lizard Pokémon',
+                },
+                {
+                    text: 'Flame Pokémon',
+                    value: 'Flame Pokémon',
+                }
+            ],
+            onFilter: (value, item) => item.classification.includes(value),
         },
         {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
+            title: 'Maximum HP',
+            dataIndex: 'maxHP',
+            key: 'maxHP',
+            sorter: (a, b) => a.maxHP - b.maxHP,
+            defaultSortOrder: 'ascend'
         },
-      ];
-      
-      return <AntTable dataSource={dataSource} columns={columns} />;
-      
-    }
+        {
+            title: 'Maximum CP',
+            dataIndex: 'maxCP',
+            key: 'maxCP',
+        },
+        {
+            title: 'Image',
+            dataIndex: 'image',
+            key: 'image',
+            render: (url) => <Image width={150} src={url} preview={false} />
+        },
+    ];
+
+    return <AntTable dataSource={dataPokemon} columns={columns} pagination={{
+        pageSize: rows,
+        // defaultPageSize: 100
+        showSizeChanger: false,
+    }} />
+}
