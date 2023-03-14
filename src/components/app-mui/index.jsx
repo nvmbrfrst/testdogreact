da322c17031df0d638a659d9cb29c03ac637e1ac
nvmbrfrst <mts89803448603@yandex.ru>
2023-03-14 17:21:47 +0300

    test

--- src/components/app-mui/index.jsx
+++ src/components/app-mui/index.jsx
@@ -1,23 +1,19 @@
-// import { Delete as DeleteIcon, RestoreFromTrash as RestoreFromTrashIcon } from '@mui/icons-material';
+import { Delete as DeleteIcon, RestoreFromTrash as RestoreFromTrashIcon } from '@mui/icons-material';
 import { Button, Container } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline';
 import GlobalStyles from '@mui/material/GlobalStyles';
 // import { postData } from '../../postData';
-// import { AppHeader } from '../app-header';
+import { AppHeader } from '../app-header';
 // import { PostList } from '../post-list';
 
 const titleGlobalStyles = <GlobalStyles styles={{ h1: { color: 'grey' } }} />
 
-
-
 export const AppMui = () => {
     return (
         <>
-
-            <Button variant='contained'>Hello World!</Button>
-
-            {/* <CssBaseline />
-            <Container>
+            <CssBaseline />
+            <AppHeader />
+            {/* <Container>
                 <AppHeader />
                 <PostList posts={postData} />
             </Container> */}
